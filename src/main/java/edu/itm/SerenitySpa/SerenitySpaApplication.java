eac8684f0fbb267b43742dbe8d55b80e50c481c7
alexxabr19 <alexabedoya1136445@correo.itm.edu.co>
2026-09-21 16:28:17 -0500

    Spring Boot

---
+++ src/main/java/edu/itm/SerenitySpa/SerenitySpaApplication.java
@@ -0,0 +1,13 @@
+package edu.itm.SerenitySpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerenitySpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerenitySpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/itm/SerenitySpa/SerenitySpaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itm.SerenitySpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerenitySpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
