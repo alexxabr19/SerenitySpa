558edbbbb21098709bd1650924e01ed3aed2348d
Fabian <fabianpena261952@correo.itm.edu.co>
2026-09-21 22:44:04 -0500

    se borró servicio y se cambió por cliente.

---
+++ src/main/java/edu/itm/SerenitySpa/identities/Cliente.java
@@ -0,0 +1,61 @@
+package edu.itm.SerenitySpa.identities;
+
+public class Cliente
+{
+    private int    idCliente;
+    private String nombre;
+    private String correo;
+    private String telefono;
+
+    //Cons vacio
+    public Cliente()
+    {    }
+
+    //Cons completo
+    public Cliente(int idCliente, String nombre, String correo, String telefono)
+    {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    //get-set
+
+    public int getIdCliente()
+    {
+        return idCliente;
+    }
+    public void setIdCliente(int idCliente)
+    {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo()
+    {
+        return correo;
+    }
+    public void setCorreo(String correo)
+    {
+        this.correo = correo;
+    }
+
+    public String getTelefono()
+    {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono)
+    {
+        this.telefono = telefono;
+    }
+}
